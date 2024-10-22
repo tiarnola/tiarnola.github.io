5ef572e14a21266a63e84fab40d0562538560322
Tiarnan Guinee <tiarnanguinee@Tiarnans-MacBook-Pro-2.local>
2024-10-22 15:16:02 +0100

    add student title

--- src/components/NameBlock/NameBlock.tsx
+++ src/components/NameBlock/NameBlock.tsx
@@ -4,7 +4,10 @@ const NameBlock = () => {
   return (
     <div className="name-block">
       <p className="headline">Tiarnan Guinee</p>
+      <div className="role-student-info">
       <h3>Full-Stack Developer</h3>
+      <h3>Quality Systems Validation with Data Analytics Student</h3>
+      </div>
       {/* <p>
         I make visually pleasing, interactive, and user-friendly digital
         experiences. 
